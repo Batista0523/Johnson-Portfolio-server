90ddc356af6da1efd1a23e458bf415ca471ef40d
elisaul batista <elisaulbatista@pursuit.org>
2023-11-21 21:58:22 -0500

    app and server.js set up

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const projectsControllers = require("./Controllers/projectsControllers.js");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/property", projectsControllers);
+app.get("/", (req, res) => {
+  res.send("Welcome to Johnson Portfolio /property to see data");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({ success: false, data: { error: "page not found" } });
+});
+
+module.exports = app;
